c4f0cbd169bd1c18c4fdf920ba903901d2ec75f6
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2022-12-06 17:53:36 +0100

    [mirotalkc2c] - save room in localStorage

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -9,13 +9,14 @@ const userNameIn = document.getElementById('userNameInput');
 const joinBtn = document.getElementById('joinBtn');
 
 function initHome() {
-    roomIdIn.value = roomId;
+    roomIdIn.value = roomId ? roomId : window.localStorage.room || '';
     userNameIn.value = window.localStorage.name || '';
 
     joinBtn.onclick = () => {
         if (roomIdIn.value && userNameIn.value) {
             const joinURL = window.location.origin + '/join?room=' + roomIdIn.value + '&name=' + userNameIn.value;
             window.history.pushState({ url: joinURL }, roomIdIn.value, joinURL);
+            window.localStorage.room = roomIdIn.value;
             window.localStorage.name = userNameIn.value;
         }
     };
