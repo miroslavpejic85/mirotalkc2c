e19ed95b348ef3199f2d8937f8cdefc5a18e3d96
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-02-20 11:16:15 +0100

    [mirotalkc2c] - fix typo

--- backend/xss.js
+++ backend/xss.js
@@ -5,9 +5,12 @@ const logs = require('./logs');
 const log = new logs('xss');
 
 const checkXSS = (dataObject) => {
-    const data = xss(JSON.stringify(dataObject));
-    log.debug('Sanitization done');
-    return JSON.parse(data);
+    if (typeof dataObject === 'object' && dataObject !== null) {
+        const data = xss(JSON.stringify(dataObject));
+        log.debug('Sanitization done');
+        return JSON.parse(data);
+    }
+    return dataObject;
 };
 
 module.exports = checkXSS;
