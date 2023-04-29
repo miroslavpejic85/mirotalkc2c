e9c6d9e02843754f79f8d8a44cbc6da1c4e66b3d
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-04-29 09:52:35 +0200

    [mirotalkc2c] - #5 make Stun optional

--- backend/server.js
+++ backend/server.js
@@ -49,13 +49,13 @@ const io = new Server({ maxHttpBufferSize: 1e7, transports: ['websocket'] }).lis
 
 const ngrokAuthToken = process.env.NGROK_AUTH_TOKEN || false;
 
-const stunServerUrl = process.env.STUN_SERVER_URL || 'stun:stun.l.google.com:19302'; // Mandatory
+const stunServerUrl = process.env.STUN_SERVER_URL;
 const turnServerUrl = process.env.TURN_SERVER_URL;
 const turnServerUsername = process.env.TURN_SERVER_USERNAME;
 const turnServerCredential = process.env.TURN_SERVER_CREDENTIAL;
-let iceServers = [{ urls: stunServerUrl }];
+let iceServers = [];
+if (stunServerUrl) iceServers.push({ urls: stunServerUrl });
 if (turnServerUrl && turnServerUsername && turnServerCredential) {
-    // Highly recommended in case the direct peer to peer connection is not possible
     iceServers.push({ urls: turnServerUrl, username: turnServerUsername, credential: turnServerCredential });
 }
 
