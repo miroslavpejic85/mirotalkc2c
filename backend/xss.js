0512a364c0acddf22aa7b92a40d342a2745ba19e
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2023-05-29 13:40:30 +0200

    [mirotlakc2c] - improve check XSS

--- backend/xss.js
+++ backend/xss.js
@@ -1,16 +1,54 @@
 'use strict';
 
 const xss = require('xss');
-const logs = require('./logs');
-const log = new logs('xss');
+const Logs = require('./logs');
+const log = new Logs('xss');
 
+/**
+ * Prevent XSS injection by client side
+ *
+ * @param {object} dataObject
+ * @returns sanitized object
+ */
 const checkXSS = (id, dataObject) => {
-    if (typeof dataObject === 'object' && Object.keys(dataObject).length > 0) {
-        const data = xss(JSON.stringify(dataObject));
-        log.debug('[' + id + '] Sanitization done');
-        return JSON.parse(data);
+    try {
+        if (typeof dataObject === 'object' && Object.keys(dataObject).length > 0) {
+            let objectJson = objectToJSONString(dataObject);
+            if (objectJson) {
+                let jsonString = xss(objectJson);
+                let jsonObject = JSONStringToObject(jsonString);
+                if (jsonObject) {
+                    log.debug('[' + id + '] XSS Object sanitization done');
+                    return jsonObject;
+                }
+            }
+        }
+        if (typeof dataObject === 'string' || dataObject instanceof String) {
+            log.debug('[' + id + '] XSS String sanitization done');
+            return xss(dataObject);
+        }
+        log.warn('[' + id + '] XSS not sanitized', dataObject);
+        return dataObject;
+    } catch (error) {
+        log.error('[' + id + '] XSS error', { data: dataObject, error: error });
+        return dataObject;
     }
-    return xss(dataObject);
 };
+
+function objectToJSONString(dataObject) {
+    try {
+        return JSON.stringify(dataObject);
+    } catch (error) {
+        return false;
+    }
+}
+
+function JSONStringToObject(jsonString) {
+    try {
+        return JSON.parse(jsonString);
+    } catch (error) {
+        return false;
+    }
+}
 
 module.exports = checkXSS;
