f7e1270827d0d153783a4844205ae5ee2f5b322a
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2024-07-29 14:05:02 +0200

    [mirotalkc2c] - fix translate

--- frontend/js/translate.js
+++ frontend/js/translate.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const script = document.createElement('script');
-script.setAttribute('async', '');
-script.setAttribute('src', 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
-document.head.appendChild(script);
+const trScript = document.createElement('script');
+trScript.setAttribute('async', '');
+trScript.setAttribute('src', 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
+document.head.appendChild(trScript);
 
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
