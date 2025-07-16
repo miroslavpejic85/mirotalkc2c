cb9941124ebd159769898f7c072bfb5959318b12
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-07-16 20:45:33 +0200

    [mirotalkc2c] - fix chat input

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -9,7 +9,7 @@
  * @license For private project or commercial purposes contact us at: license.mirotalk@gmail.com or purchase it directly via Code Canyon:
  * @license https://codecanyon.net/item/mirotalk-c2c-webrtc-real-time-cam-2-cam-video-conferences-and-screen-sharing/43383005
  * @author  Miroslav Pejic - miroslav.pejic.85@gmail.com
- * @version 1.2.20
+ * @version 1.2.21
  */
 
 const roomId = new URLSearchParams(window.location.search).get('room');
@@ -172,6 +172,7 @@ let isPushToTalkActive = false;
 let isSpaceDown = false;
 let isMyAudioActiveBefore = false;
 let isMyVideoActiveBefore = false;
+let isChatPasteTxt = false;
 let noiseProcessor = null;
 let localMediaStream = null;
 let remoteMediaStream = null;
@@ -1066,10 +1067,15 @@ function handleEvents() {
         }
     };
     chatInput.oninput = function () {
+        //if (isChatPasteTxt) return;
         for (let i in chatInputEmoji) {
             let regex = new RegExp(i.replace(/([()[{*+.$^\\|?])/g, '\\$1'), 'gim');
             this.value = this.value.replace(regex, chatInputEmoji[i]);
         }
+        checkLineBreaks();
+    };
+    chatInput.onpaste = () => {
+        isChatPasteTxt = true;
         checkLineBreaks();
     };
     leaveBtn.onclick = () => {
